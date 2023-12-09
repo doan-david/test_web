020ca484380c7586119a95b7dcb3dd536563f3fc
Hoàng Đoàn <doanhgminh@gmail.com>
2023-12-09 08:42:51 +0700

    stt

---
+++ application/views/frontend/modules/footer.php
@@ -0,0 +1,75 @@
+<footer id="footer">
+    <div class="news-social">
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <ul class="list-unstyled social pull-right">
+                  <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                  <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                </ul>
+              </div>
+        </div>
+    </div>
+    <div class="top-footer">
+        <div class="container">
+            <div class="col-xs-12 col-sm-12 col-md-6">
+                <div class="col-xs-12 col-sm-6">
+                    <a href="/">
+                        <img src="public/images/logo-new-04.png" class="img-fix f-logo" alt="green-construction">
+                    </a>
+                   <div class="about-store">
+                    Web nông nghiệp hữu cơ đem đến sức khỏe cho mọi nhà.
+                   </div>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <div class="f-contact">
+                        <h3>Thông tin liên hệ</h3>
+                        <ul class="list-unstyled">
+                            <li class="clearfix">
+                                <i class="fa fa-map-marker"></i>
+                                <span>ĐH điện lực</span>
+                            </li>
+                            <li class="clearfix">
+                                <i class="fa fa-phone"></i>
+                                <span>0123465678</span>
+                            </li>
+                            <li class="clearfix">
+                                <i class="fa fa-envelope"></i>
+                                <span><a href="mailto:sale.24hstore@gmail.com">doanhgminh@gmail.com</a></span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6">
+                <div class="col-xs-12 col-sm-6">
+                    <h3 class="footer-title">Chính sách</h3>
+                    <ul class="list-unstyled linklists">
+                        <li><a href="chinh-sach">Chính sách thanh toán</a></li>          
+                        <li><a href="chinh-sach">Chính sách vận chuyển</a></li>
+                        <li><a href="chinh-sach">Chính sách đổi trả</a></li>           
+                        <li><a href="chinh-sach">Chính sách bảo hành</a></li>           
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <h3 class="footer-title">Điều khoản</h3>
+                    <ul class="list-unstyled linklists">
+                        <li><a href="dieu-khoan">Điều khoản sử dụng</a></li>           
+                        <li><a href="dieu-khoan">Điều khoản giao dịch</a></li>           
+                        <li><a href="dieu-khoan">Dịch vụ tiện ích</a></li>           
+                        <li><a href="dieu-khoan">Quyền sở hữu trí tuệ</a></li>           
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="payments">
+        <div class="container">
+            <img src="public/images/payment.png" alt="Phương thức thanh toán" class="img-fix">
+        </div>
+    </div>
+    <div class="copyright">
+        © Bản quyền thuộc về Web nông nghiệp hữu cơ 
+    </div>
+</footer>
